133e948b60e061ab5d2c5454217e683d2a58c66b
HenryChinask1 <jeffbaier72@yahoo.com>
2025-02-26 13:59:37 -0500

    Added more of the first class

--- CoffeeShopSim.py
+++ CoffeeShopSim.py
@@ -76,6 +76,54 @@ class CoffeeShopSim:
             # Display the day with a fancy header.
             self.dayHeader()
 
+            # Get the weather for the day.
+            temp = self.weather
+
+            # Display the cash and the weather.
+            self.dailyStats(temp)
+
+            # Get the price of a cup of coffee.
+            cupPrice = float(prompt("What do you want to charge for a cup of coffee? "))
+
+            # Get the advertising spend.
+            print('\nYou can buy advertising to help boost sales.')
+            advertising = float(prompt("How much do you want to spend on advertising? (0 for None) ", False))
+            advertising = convertToFloat(advertising)
+            self.cash -= advertising
+
+            # Simulate the sales for the day.
+            cupsSold = self.simulate(temp, advertising, cupPrice)
+            grossProfit = cupsSold * cupPrice
+
+            # Display the results.
+            print(f'\nYou sold {cupsSold} cups of coffee today.')
+            print(f'You made ${grossProfit}.')
+            self.cash += grossProfit
+            self.coffeeInv -= cupsSold
+            self.incDay()
+
+    def siumulate(self, temp, advertising, cupPrice):
+        # Calculate cups sold.
+        cupsSold = self.dailySales(temp, advertising)
+
+        # Save the sales results for the day.
+        self.sales.append({"day": self.day,
+                           'coffeeInv': self.coffeeInv,
+                           'advertising': advertising,
+                           'temperature': temp,
+                           'cupPrice': cupPrice,
+                           'cupsSold': cupsSold})
+        return cupsSold
+    
+    def setTemps(self):
+        temps = []
+
+        avg = (self.TEMPMIN + self.TEMPMAX) / 2
+        maxDistFromAvg = self.TEMPMAX - avg
+
+        for i in range(self.TEMPMIN, self.TEMPMAX):
+            distFromAvg = abs(avg - i)
+
 def dailyStats(cashOnHand, weather, coffeeInv):
     print(f'You have ${cashOnHand:.2f} cash on hand and the temperature is {weather} degrees.')
     print(f'You have enough coffee on hand to make {coffeeInv} cups of coffee.\n')
